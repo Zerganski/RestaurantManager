f581bc8ef17e5938197f572c318920e1d5564101
piotr <piotr.zaszczysnki@gmail.com>
2022-08-31 20:17:56 +0200

    AnimalSpecies update

--- src/main/java/com/animal/AnimalSpecies.java
+++ src/main/java/com/animal/AnimalSpecies.java
@@ -59,3 +59,62 @@ class Animals
     static int chromosomeNumberLobster = 136;
     static String descriptionLobster = "Lobsters have compound eyes on movable stalks, two pairs of long antennae, and several pairs of swimming legs (swimmerets) on the elongated abdomen.";
 }
+
+/*
+    class Animals
+    {
+    String animalName;
+    String speciesName;
+    int chromosomeNumber;
+    String description;
+    }
+
+    public static void main(String[] args) {
+
+    Animals hawk = new Animals();
+    Animals bear = new Animals();
+    Animals lobster = new Animals();
+
+    hawk.animalName = "hawk";
+    hawk.speciesName = "bird";
+    hawk.chromosomeNumber = 80;
+    hawk.description = "Hawks are strong, powerful birds. Their feet are equipped with sharp, curved talons for capturing prey, and their strong beaks are hooked for biting and tearing flesh.";
+
+    bear.animalName = "bear";
+    bear.speciesName = "mammal";
+    bear.chromosomeNumber = 74;
+    bear.description = "Big mammal with thick fur that hibernate in winter.";
+
+    lobster.animalName = "lobster";
+    lobster.speciesName = "arthropod";
+    lobster.chromosomeNumber = 136;
+    lobster.description = "Lobsters have compound eyes on movable stalks, two pairs of long antennae, and several pairs of swimming legs (swimmerets) on the elongated abdomen.";
+
+    Scanner in = new Scanner(System.in);
+    System.out.print("Which animal specie would you like to get information of?: \n 1. Hawk \n 2. Bear \n 3. Lobster \n Your input: ");
+    int answer = in.nextInt();
+    switch (answer) {
+            case 1:
+            System.out.print("Animal name is: " + hawk.animalName + "\n");
+            System.out.print("Animal specie is: " + hawk.speciesName + "\n");
+            System.out.print("Animal haploid chromosome number is: " + hawk.chromosomeNumber/2 + "\n");
+            System.out.print("Animal description is: " +  hawk.description + "\n");
+            break;
+
+           case 2:
+            System.out.print("Animal name is: " + bear.animalName + "\n");
+            System.out.print("Animal specie is: " +  bear.speciesName + "\n");
+            System.out.print("Animal haploid chromosome number is: " + bear.chromosomeNumber/2 + "\n");
+            System.out.print("Animal description is: " + bear.description + "\n");
+            break;
+
+
+            case 3:
+            System.out.print("Animal name is: " + lobster.animalName + "\n");
+            System.out.print("Animal specie is: " + lobster.speciesName + "\n");
+            System.out.print("Animal haploid chromosome number is: " + lobster.chromosomeNumber/2 + "\n");
+            System.out.print("Animal description is: " + lobster.description + "\n");
+            break;
+            }
+        }
+ */
