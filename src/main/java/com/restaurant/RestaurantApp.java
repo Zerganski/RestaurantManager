009477a504788ce03fa5549fbac269ebb662956e
wroneuke <wroneuke@gmail.com>
2023-07-17 17:12:13 +0200

    added ToDoList - RestaurantApp class

--- src/main/java/com/restaurant/RestaurantApp.java
+++ src/main/java/com/restaurant/RestaurantApp.java
@@ -1,31 +1,18 @@
 package com.restaurant;
 
 /*
-Restaurant manager:
-2. Create a console restaurant system, which allows you to create a new restaurant and meal for a particular restaurant.
-The restaurant should contain id, name, address, type, Meal should have name, price and id.
-Ids are assigned by the program during entity creation. Initially create a structure containing 3 restaurants with data, data should be available from the program start:
-Kebab u grubego; Warszawska 11, Kraków 31-222; TURKISH
-Milano Pizza; Sobczyka 12/3, Warszawa 33-312; ITALIAN
-Meat Burger; Lipowa 31b/1 Gdańsk 18-111; AMERICAN
-
-For program execution use IntellIJ (run or debug program):
-At the beginning the program prints instructions, after each point user input is expected.
-If you want to exit, type “exit”
-If you want to create a restaurant, type “1”, if the user chooses this flow, then
-Type restaurant name
-Type restaurant address
-Type restaurant type[ASIAN, ITALIAN, FRENCH, AMERICAN, TURKISH], nice to have - validation for those values
-Show restaurant information is added with the restaurant id(which may be important later)
-If you want to add a meal to a restaurant, type “2”, if the user chooses this flow, then
-Type meal name
-Type meal price
-Type restaurant id to add the meal to a particular restaurant(most likely the user needs to print all available restaurants previously)
-If user types “3” Show all restaurants, if user chooses this flow, then all restaurants should be printed
-If the user types “4”,  shows all meals in a particular restaurant , if the user chooses this flow, then:
-Type restaurant id(should be shown in restaurant print),
-All meals of a specific restaurant should be printed
-If a user types “5”, he should changes the restaurant name to the restaurant he chooses using id.
+1. Change name of project in GitHub - RestaurantManager, RestaurantApp.
+   choose one which will fit good in your opinion, not restaurant-karol XD
+2. Create a new branch, call it refactoring. You can't create commits in master branch - safety things.
+   after refactoring stuff you should create a pull request and I will check your changes.
+3. Create packages and move classes to new packages.
+    model - Meal, Restaurant
+    storage - RestaurantRepository
+    consumer - RestaurantApp, RestaurantProcessor
+4. Create a new service class (RestaurantServiceImpl) and move all methods from RestaurantRepository.
+5. Create an interface for service class (RestaurantService)
+6. Create a new package for service class and interface - call it service
+6. Delete all unused methods and constructors.
  */
 public class RestaurantApp {
 
